a7f29fa04a9d3858275c9c9569454fc175537644
Vladyslav Bilonoh <maodzhedun@gmail.com>
2025-05-28 13:08:32 +0300

    first commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,80 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
 
-export const App = () => {
-  return <div>goit-react-hw-02-phonebook</div>;
-};
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { name, number, contacts } = this.state;
+    console.log(name);
+    console.log(contacts);
+    console.log(number);
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    console.log(newContact);
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+    }));
+  };
+
+  render() {
+    const { contacts, name, number } = this.state;
+
+    return (
+      <div>
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                value={name}
+                required
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <label>
+              Number
+              <input
+                type="text"
+                name="number"
+                value={number}
+                onChange={this.handleInputChange}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </div>
+        <div>
+          <h2>Contacts</h2>
+          <ul>
+            {contacts.map(contact => (
+              <li key={contact.id}>{contact.name}: {contact.number}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
